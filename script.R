6c4849870f7483bf63862452c7c41a39e4055b0a
Armel Brizuela Rodríguez <121496069+armelbrizuela@users.noreply.github.com>
2024-06-20 07:22:02 -0600

    Update script.R

--- script.R
+++ script.R
@@ -1,3 +1,3 @@
 datos <- foreign::read.spss(
-  https://github.com/armelbrizuela/datos/raw/main/sexismos.sav, 
+  'https://github.com/armelbrizuela/datos/raw/main/sexismos.sav', 
   to.data.frame = T)
